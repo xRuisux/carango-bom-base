43137d41448af471f0825d5489584718238ec924
Tailine Chagas <tailinechagas@gmail.com>
2021-08-17 15:35:06 -0300

    refactor: put routes in separated file

---
+++ src/routes.js
@@ -0,0 +1,27 @@
+import { Switch, Route, BrowserRouter as Router } from "react-router-dom";
+import CadastroMarca from "./pages/CadastroMarca";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+
+export default function Routes() {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/cadastro-marca">
+          <CadastroMarca></CadastroMarca>
+        </Route>
+        <Route path='/alteracao-marca/:id'>
+          <CadastroMarca></CadastroMarca>
+        </Route>
+        <Route path="/veiculos">
+          <div>Veículos</div>
+        </Route>
+        <Route path="/login" component={Login} />
+        {/* <Route path="/">
+          <ListagemMarcas></ListagemMarcas>
+        </Route> */}
+        <Route exact path="/" component={Home} />
+      </Switch>
+    </Router>
+  )
+}
