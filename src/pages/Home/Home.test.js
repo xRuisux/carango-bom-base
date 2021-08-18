fcac789e714362d30a1aeb0842398cbfeafbe0b9
Tailine Chagas <tailinechagas@gmail.com>
2021-08-18 17:33:13 -0300

    refactor: rename file and delete duplicated test assertion

--- src/pages/Home/Home.test.js
+++ src/pages/Home/Home.test.js
@@ -1,22 +1,21 @@
 import { screen } from "@testing-library/react"
-import Home from ".."
-import { renderWithRouter } from "../../../utils/renderWithRouter"
+import Home from "."
+import { renderWithRouter } from "../../utils/renderWithRouter"
 import '@testing-library/jest-dom';
 
 describe("<Home />", () => {
   it("render correctly", () => {
     renderWithRouter(<Home />)
 
-    expect(screen.getByRole('heading', {name: 'Bem vinda ao Carango Bom!'})).toBeInTheDocument()
-    expect(screen.getByRole('heading', {name: 'Bem vinda ao Carango Bom!'}))
+    expect(screen.getByRole('heading', { name: 'Bem vinda ao Carango Bom!' })).toBeInTheDocument()
+    expect(screen.getByRole('heading', { name: 'Bem vinda ao Carango Bom!' }))
     expect(screen.getByRole('link', { name: /visualizar veículos/i })).toBeInTheDocument()
-    expect(screen.getByRole('link', { name: /efetuar login/i })).toBeInTheDocument()
     expect(screen.getByRole('link', { name: /efetuar login/i })).toBeInTheDocument()
   })
 
   it("should have a link to veicules and login pages", () => {
     renderWithRouter(<Home />)
-    
+
     expect(screen.getByRole('link', { name: /visualizar veículos/i })).toHaveAttribute("href", "/veiculos")
     expect(screen.getByRole('link', { name: /efetuar login/i })).toHaveAttribute("href", "/login")
   })
