00c8beffcfb861bde47990f341a5e9d16ee85cca
Luiz Lima <luiz.c.brito1@gmail.com>
2021-09-03 10:53:42 -0300

    fix: add confirm

--- src/pages/BrandList/BrandList.jsx
+++ src/pages/BrandList/BrandList.jsx
@@ -2,15 +2,20 @@ import React, { useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import Table from '../../components/Table/Table';
 import BrandService from '../../services/BrandService';
+import { Confirm } from "../../components/Confirm/Confirm"
+import { delayFunc } from "../../utils/delayFunc"
 
 const columns = [
     { field: 'name', headerName: 'Marca', width: 200 }
 ];
 
 function BrandList() {
-    const [brands, setBrands] = useState([]);
-    const [brandSelected, setBrandSelected] = useState();
     const history = useHistory();
+    const [error, setError] = useState('')
+    const [brands, setBrands] = useState([]);
+    const [loading, setLoading] = useState(false)
+    const [brandSelected, setBrandSelected] = useState();
+    const [isConfirmOpen, setIsConfirmOpen] = useState(false)
 
     function create() {
         history.push('/create-brand/');
@@ -24,9 +29,18 @@ function BrandList() {
     }
 
     async function remove() {
+        setLoading(true)
+
         await BrandService.delete(brandSelected);
         setBrandSelected(null);
         loadBrands();
+
+        setIsConfirmOpen(false)
+        delayFunc(() => setLoading(false))
+    }
+
+    function handleDelete() {
+        setIsConfirmOpen(true)
     }
 
     useEffect(() => loadBrands(), []);
@@ -38,17 +52,22 @@ function BrandList() {
 
     return (
         <>
-            <div style={{ height: 300, width: '100%' }}>
+        <Confirm open={isConfirmOpen} message='Deseja mesmo excluir a marca?' onConfirm={remove} onCancel={() => setIsConfirmOpen(false)} />
+        {
+            !!error ? <p>{error}</p> 
+            : <div style={{ height: 300, width: '100%' }}>
                 <Table
                     rows={ brands }
                     columns={ columns }
                     addItem={ create }
                     updateItem={ update }
-                    deleteItem={ remove }
+                    deleteItem={ handleDelete }
                     selectedItem={ brandSelected }
                     rowSelectedFunction={ setBrandSelected }
                 />
             </div>
+            }
+
         </>
     );
 }
